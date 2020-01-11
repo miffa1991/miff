d9c07947b26e3a056e1046e35a6b0c1daef31a55
Vadim <mifa912088@gmail.com>
2020-01-11 12:49:56 +0200

    header del link

--- header.php
+++ header.php
@@ -15,14 +15,12 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
 
 	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'miff' ); ?></a>
 
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
@@ -45,7 +43,6 @@
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'miff' ); ?></button>
 			<?php
 			wp_nav_menu( array(
 				'theme_location' => 'menu-1',
